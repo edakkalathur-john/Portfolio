f358244672b6d381dac3cd0f1b47176d03d41e09
Benhar John <BenharEJ@gmail.com>
2025-05-27 01:03:49 +0000

    can you load the website with the theme of users theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 'use client'; // 'use client' is necessary for ThemeProvider at the root
 
 // Removed Geist font imports as they were causing issues and system fonts are used as fallback.
@@ -17,7 +16,7 @@ export default function RootLayout({
       {/* Apply font variables if Geist fonts are re-introduced and working: */}
       {/* className={`${GeistSans.variable} ${GeistMono.variable}`} */}
       <body className="bg-white text-black font-display tracking-widest dark:bg-black dark:text-white antialiased" suppressHydrationWarning>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
